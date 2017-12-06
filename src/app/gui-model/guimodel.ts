c785b4dbb0d419f51c6861850a8532dd44a66b67
Corina Kälin <kaelico2@students.zhaw.ch>
2017-12-06 12:51:57 +0100

    updated

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -1,15 +1,15 @@
 export class GuiModel {
-
+ 
     /* HINWEIS: Texte sind in der Datei ../example-translation-service.ts definiert.
     Erscheinen Texte in {}, so kann die Übersetzung in example-translation-service.ts definiert werden
-
+ 
     Key: Bei Listen-Daten kann ein Key mitgegeben werden. Wird kein Key mitgegeben, dann werden nach Klick
     des Zurück-Buttons die Elemente nicht mehr korrekt dargestellt.
     */
-
+ 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Corinas Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
@@ -29,12 +29,27 @@ export class GuiModel {
                             "width": 1,
                             "required": true
                         },
+						{
+							"id": "nickname",
+                            "type": "text",
+                            "name": "Nickname",
+                            "width": 2,
+                            "required": true
+						},
+                        {
+                            "id": "group",
+                            "type": "autocomplete",
+                            "name": "Group",
+                            "data": [ "Study", "Family", "School" ],
+                            "form": "GroupForm",
+                            "width": 2
+                        },
                         {
                             "id":   "location",
                             "type": "autocomplete",
                             "name": "Location",
                             "data": [ "Winterthur", "Zürich" ],
-                            "form": "GroupForm",
+                            "form": "LocationForm",
                             "width": 2
                         },
                         {
@@ -62,9 +77,11 @@ export class GuiModel {
                         {
                             "type": "okButton",
                             "name": "Ok"
-                        }
+                        },
+                                                                                   
                     ]
                 },
+				
                 {
                     "id": "LocationForm",
                     "title": "Location",
@@ -75,6 +92,14 @@ export class GuiModel {
                             "name": "LocationName",
                             "width": 2,
                             "required": true
+                        },
+						{
+                            "id": "comment",
+                            "type": "text",
+                            "name": "Comments",
+                            "width": 2,
+                            "height": 4,
+                            "maxLength": 5000,
                         },
                         {
                             "type": "deleteButton",
@@ -89,8 +114,103 @@ export class GuiModel {
                             "name": "Ok"
                         }
                     ]
+                },
+				
+				
+				{
+                    "id": "ActivityForm",
+                    "title": "Activity",
+                    "formFieldList": [
+                        {
+                            "id": "name",
+                            "type": "text",
+                            "name": "Name Actitity",
+                            "width": 2,
+                            "required": true
+                        },
+						{
+                            "id": "name",
+                            "type": "date",
+                            "name": "Date",
+                            "width": 2
+                        },
+						{
+                            "id": "comment",
+                            "type": "text",
+                            "name": "Comments",
+                            "width": 2,
+                            "height": 4,
+                            "maxLength": 5000,
+                        },
+						{
+                            "id":   "location",
+                            "type": "autocomplete",
+                            "name": "Location",
+                            "data": [ "Winterthur", "Zürich" ],
+                            "form": "LocationForm",
+                            "width": 2
+                        },
+                        {
+                            "type": "deleteButton",
+                            "name": "Delete"
+                        },
+                        {
+                            "type": "cancelButton",
+                            "name": "Cancel"
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                    ]
+                },
+				
+				
+                {
+					"id": "GroupForm",
+                    "title": "Group",
+                    "formFieldList": [
+                        {
+                            "id": "name",
+                            "type": "text",
+                            "name": "GroupName",
+                            "width": 2,
+                            "required": true,
+                        },
+						{
+                            "id": "name",
+                            "type": "date",
+                            "name": "CreationDate",
+                            "width": 2,
+                            "required": true,
+                        },
+						{
+                            "id": "comment",
+                            "type": "text",
+                            "name": "Comments",
+                            "width": 2,
+                            "height": 4,
+                            "maxLength": 5000,
+                        },
+                        {
+                            "type": "deleteButton",
+                            "name": "Delete"
+                        },
+                        {
+							"type": "cancelButton",
+                            "name": "Cancel"
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                     ]
                 }
             ],
+			
+			
+			
+			
             "pageList": [
                 {
                     "id": "mainmenu",
@@ -99,19 +219,70 @@ export class GuiModel {
                         {
                             "type": "button",
                             "name": "Friends",
-                            "icon": "fa-user",
-                            "color": "blue",
+                            "icon": "fa-facebook-square",
+                            "color": "magenta",
                             "page": "friendspage",
                         },
                         {
                             "type": "button",
                             "name": "Location",
-                            "icon": "fa-cubes",
-                            "color": "yellow",
+                            "icon": "fa-home",
+                            "color": "peter river",
                             "page": "locationspage",
+                        },
+                        {
+                            "type": "button",
+                            "name": "Groups",
+                            "icon": "fa-gavel",
+                            "color": "red",
+                            "page": "groupspage",
+                        },
+						{
+                            "type": "button",
+                            "name": "Activity",
+                            "icon": "fa-bath",
+                            "color": "silver",
+                            "page": "activitypage",
+                        },
+ 
+                    ]
+                },
+                {
+                    "id": "groupspage",
+                    "elementList": 
+					[
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "NewGroup",
+                            "icon": "fa-gavel",
+                            "color": "alizarin",
+                            "form": 
+							{
+                                "form": "GroupForm"
+                            }
+                        },
+						
+
+                        {
+                            "type": "list",
+                            "icon": "fa-gavel",
+                            "color": "alizarin",
+                            "search": true,
+                            "data": [ {name: "Study"}, {name: "Family"}, {name: "School"}],
+                            "form": 
+							{
+								"form": "GroupForm"
+                            }
                         },
                     ]
                 },
+				
+				
+				
+				
                 {
                     "id": "friendspage",
                     "elementList": [
@@ -121,24 +292,88 @@ export class GuiModel {
                         {
                             "type": "newButton",
                             "name": "NewFriend",
-                            "icon": "fa-user",
-                            "color": "green",
+                            "icon": "fa-facebook-square",
+                            "color": "pink",
                             "form": {
                                 "form": "FriendForm"
                             }
                         },
                         {
                             "type": "list",
-                            "icon": "fa-user",
-                            "color": "blue",
+                            "icon": "fa-facebook-square",
+                            "color": "purple",
                             "search": true,
                             "data": [ { name: "Anton Amacker" }, { name: "Britta Beavers"} ],
+                            "page": "friendpage"
+                        },
+                    ]
+                },
+				
+				{
+                    "id": "friendpage",
+                    "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "EditFriend",
+                            "icon": "fa-facebook-square",
+                            "color": "amethyst",
+                            "form": {
+                                "form": "FriendForm"
+                            }
+                        },
+						 {
+                            "type": "newButton",
+                            "name": "AddActivity",
+                            "icon": "fa-bath",
+                            "color": "silver",
+                            "form": {
+                                "form": "ActivityForm"
+                            }
+                        },
+                        {
+                            "type": "list",
+                            "icon": "fa-bath",
+                            "color": "blue",
+                            "search": true,
+                            "data": [ { name: "Schwimmen" }, { name: "Velofahren"} ],
                             "form": {
                                 "form": "FriendForm"
                             }
                         },
                     ]
                 },
+				
+				{
+                    "id": "activitypage",
+                    "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "NewActivity",
+                            "icon": "fa-bath",
+                            "color": "clouds",
+                            "form": {
+                                "form": "ActivityForm"
+                            }
+                        },
+                        {
+                            "type": "list",
+                            "icon": "fa-bath",
+                            "color": "clouds",
+                            "search": true,
+                            "data": [ { name: "Schwimmen" }, { name: "Rennen"}, { name: "Velofahren"} ],
+                            "form": {
+                                "form": "ActivityForm"
+                            }
+                        },
+                    ]
+                },
+				
                 {
                     "id": "locationspage",
                     "elementList": [
@@ -149,7 +384,7 @@ export class GuiModel {
                             "type": "newButton",
                             "name": "NewLocation",
                             "icon": "fa-home",
-                            "color": "green",
+                            "color": "belize hole",
                             "form": {
                                 "form": "LocationForm"
                             }
@@ -157,7 +392,7 @@ export class GuiModel {
                         {
                             "type": "list",
                             "icon": "fa-home",
-                            "color": "blue",
+                            "color": "peter river",
                             "search": true,
                             "data": [ { name: "Adelboden" }, { name: "Winterthur" }, { name: "Zinal"}, { name: "Zürich"} ],
                             "form": {
@@ -169,8 +404,8 @@ export class GuiModel {
             ]
         }
     };
-
-
+ 
+ 
     get guiModel() {
         return this._guiModel;
     }
